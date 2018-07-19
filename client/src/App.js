efb1b0b9425eb053d62d95af2b164701fcb9b002
Alex Yovev <ayovev511@gmail.com>
2018-07-19 09:19:37 -0700

    restructing folders and files, WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import Table from './components/table'
+import Table from './components/Table/Table.js'
 
 var dataResponse = [
   {
