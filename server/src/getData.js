7b871edc2840e07a8c1fe2ba074d966fc2c51923
Alex Yovev <ayovev511@gmail.com>
2018-07-26 17:05:51 -0700

    refactored date/period operations (moment.js) for getting data

--- server/src/getData.js
+++ server/src/getData.js
@@ -1,9 +1,7 @@
 require('dotenv').config({path: '../../.env'});
 const assert = require('assert').strict;
 const rp = require('request-promise');
-
-// TODO [Alex] Use moment.js instead of custom Date manipulations
-// const moment = require('moment');
+const moment = require('moment');
 
 // TODO [Anyone] Move to constants file and modify references?
 const _YTD = 'Year To Date';
@@ -12,18 +10,6 @@ const _1_YEAR = '1 Year';
 const _3_YEARS = '3 Years';
 const _5_YEARS = '5 Years';
 const _10_YEARS = '10 Years';
-
-// Remove prototype, make regular function
-Date.prototype.fix = function() {
-  let day = this.getDay();
-  if(day === 6) {
-    this.setDate((this.getDate() - 1));
-  }
-  else if(day === 0) {
-    this.setDate((this.getDate() - 2));
-  }
-  return this;
-}
 
 let options = {
   url: `https://www.alphavantage.co/query?`,
@@ -52,6 +38,7 @@ rp(options).then(response => {
 
   console.log(runAll(data));
 })
+
 // TODO [Alex] Come up with better / more descriptive error messaging
 .catch(error => {
   console.log(`Promise error ${error}`);
@@ -72,16 +59,13 @@ function runAll(data) {
     performance3Years: run(data, _3_YEARS),
     performance5Years: run(data, _5_YEARS),
     performance10Years: run(data, _10_YEARS)
-  }
+  };
 }
 
 function run(data, period) {
   const dates = configureDateRange(period);
   let to = dates.to;
   let from = dates.from;
-
-  to = formatDate(to);
-  from = formatDate(from);
 
   let newPrice = data['Time Series (Daily)'][to][`5. adjusted close`];
   let oldPrice = data['Time Series (Daily)'][from][`5. adjusted close`];
@@ -93,40 +77,17 @@ function run(data, period) {
 }
 
 function configureDateRange(period) {
-  let to = new Date();
-  to.setDate(to.getDate() - 1);
-
-  let from = new Date(to);
+  let to = moment().subtract(1, 'day');
+  let from = moment();
   setPeriod(from, period);
 
-  to.fix();
-  from.fix();
+  toWeekday(to);
+  toWeekday(from);
 
   return {
-    to: to,
-    from: from
+    to: to.format('YYYY-MM-DD'),
+    from: from.format('YYYY-MM-DD')
   }
-}
-
-function formatDate(date) {
-  let string = ``;
-
-  string += `${date.getFullYear()}`;
-  string += `-`;
-  if(date.getMonth() < 10) {
-    string += `0${date.getMonth() + 1}`;
-  }
-  else {
-    string += `${date.getMonth() + 1}`;
-  }
-  string += `-`;
-  if(date.getDate() < 10) {
-    string += `0${date.getDate()}`;
-  }
-  else {
-    string += `${date.getDate()}`;
-  }
-  return string;
 }
 
 function calculateReturn(period, newPrice, oldPrice) {
@@ -158,7 +119,7 @@ function calculateNonAnnualizedReturn(newPrice,oldPrice) {
   return (newPrice-oldPrice)/oldPrice;
 }
 
-function calculateAnnualizedReturn(newPrice,oldPrice,period) {
+function calculateAnnualizedReturn(newPrice, oldPrice, period) {
   let cumulativeReturn = 1 + (newPrice - oldPrice) / oldPrice;
   let holdingPeriod = 365 / (period * 365);
   let annualizedReturn = cumulativeReturn ** holdingPeriod - 1;
@@ -168,25 +129,36 @@ function calculateAnnualizedReturn(newPrice,oldPrice,period) {
 function setPeriod(from, period) {
   switch (period) {
     case _YTD:
-      from.setFullYear(from.getFullYear() - 1);
-      from.setMonth(11);
-      from.setDate(31);
+      from.subtract(1, 'year')
+      from.month(11);
+      from.date(31);
       break;
     case _1_MONTH:
-      from.setMonth(from.getMonth()-1);
+      from.subtract(1, 'month');
       break;
     case _1_YEAR:
-      from.setFullYear(from.getFullYear() - 1);
+      from.subtract(1, 'year');
       break;
     case _3_YEARS:
-      from.setFullYear(from.getFullYear() - 3);
+      from.subtract(3, 'years');
       break;
     case _5_YEARS:
-      from.setFullYear(from.getFullYear() - 5);
+      from.subtract(5, 'years');
       break;
     case _10_YEARS:
-      from.setFullYear(from.getFullYear() - 10);
+      from.subtract(10, 'years');
       break;
   }
   return from;
+}
+
+function toWeekday(date) {
+  let day = date.day();
+  if(day === 6) {
+    date.subtract(1, 'day');
+  }
+  else if(day === 0) {
+    date.subtract(2, 'days');
+  }
+  return date;
 }
