24cf3e12f2a8a6c2ad0561134e07829d6255dec5
Alex Yovev <ayovev511@gmail.com>
2018-12-01 12:24:15 -0800

    modified AuthenticationContext slightly in App component

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -2,55 +2,50 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { AuthConsumer } from "./AuthContext";
+import { AuthenticationConsumer } from "./AuthenticationContext";
 import Routes from "./Routes";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
+export default class App extends Component {
   render() {
     return (
-      <AuthConsumer>
-        {({ isAuthenticated }) => (
+      <AuthenticationConsumer>
+        {({ logout, isAuthenticated }) => (
           <div className="App container">
             <Navbar fluid collapseOnSelect>
               <Navbar.Header>
                 <Navbar.Brand>
                   <Link to="/">Home</Link>
                 </Navbar.Brand>
-
                 {isAuthenticated && (
                   <Navbar.Brand>
                     <Link to="/tables">Tables</Link>
                   </Navbar.Brand>
                 )}
-
                 <Navbar.Toggle />
               </Navbar.Header>
               <Navbar.Collapse>
                 <Nav pullRight>
-                  {!isAuthenticated && (
+                  {!isAuthenticated &&
                     <LinkContainer to="/signup">
                       <NavItem>Signup</NavItem>
                     </LinkContainer>
-                  )}
-                  {!isAuthenticated && (
+                  }
+                  {!isAuthenticated &&
                     <LinkContainer to="/login">
                       <NavItem>Login</NavItem>
                     </LinkContainer>
-                  )}
+                  }
+                  {isAuthenticated &&
+                    <NavItem onClick={logout}>Logout</NavItem>
+                  }
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
             <Routes />
           </div>
         )}
-      </AuthConsumer>
+      </AuthenticationConsumer>
     );
   }
 }
-
-export default App;
